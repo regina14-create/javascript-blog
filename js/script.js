33bbdcebabeb8b346f8b2564e240ec5c4bdd102e
regina14-create <ivaskovaregina@gmail.com>
2025-06-06 16:03:12 +0200

    Add active class to clicked article link Dodanie aktywnej klasy do klikniętego linku artykułu

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
 
   console.log(event);
   
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
+
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
+
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+
   const activeArticles = document.querySelectorAll('.posts .post.active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
-  /* remove class 'active' from all articles */
+  console.log('clickedElement:', clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
 
